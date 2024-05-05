9c03412e69b02caf80ac9d45dceec5307076ae0c
ashrafalmayah <ashrafalmayah00@gmail.com>
2024-05-05 16:01:19 +0300

    build a modal component

---
+++ src/components/Modal.vue
@@ -0,0 +1,28 @@
+<script setup>
+defineProps({
+  show: {
+    type: Boolean,
+    default: false
+  }
+})
+</script>
+
+<template>
+  <div v-if="show" @click.self="$emit('close')" class="fixed inset-0 bg-black bg-opacity-60 text-black grid place-items-center">
+    <div class="bg-white rounded-xl py-4 p-8 max-w-96 w-[80vw]">
+      <header v-if="$slots.header" class="font-bold text-xl mb-4">
+        <slot name="header" />
+      </header>
+      <div>
+        <slot>Default Content</slot>
+      </div>
+      <footer class="mt-4 pt-2 border-t-2 flex justify-end">
+        <slot name="footer">
+          <button @click="$emit('close')" class="bg-gray-600 text-sm hover:bg-gray-700 text-white rounded-2xl py-1 px-6">
+            Close
+          </button>
+        </slot>
+      </footer>
+    </div>
+  </div>
+</template>
--- src/views/TeamView.vue
+++ src/views/TeamView.vue
@@ -2,24 +2,35 @@
 import TeamHeader from '@/components/Teams/TeamHeader.vue'
 import TeamMembers from '@/components/Teams/TeamMembers.vue'
 import TeamFooter from '@/components/Teams/TeamFooter.vue'
-// import team from '@/team.json'
+import Modal from '@/components/Modal.vue'
 import { useTeamStore } from '@/stores/TeamStore'
+import { ref } from 'vue'
 
 let team = useTeamStore()
+
+let showModal = ref(false)
 
 team.fill()
 
 setTimeout(() => {
   team.grow(25)
-}, 3000);
+}, 3000)
 </script>
 
 <template>
-  <TeamHeader />
+  <TeamHeader @add="showModal = true" />
 
   <div class="place-self-center flex flex-col gap-y-3" style="width: 725px">
     <TeamMembers />
   </div>
 
   <TeamFooter />
+
+  <Modal @close="showModal = false" :show="showModal">
+    <template #header>Add a Member</template>
+    <form action="#" class="flex gap-4">
+      <input type="text" placeholder="Email Address..." class="px-2 flex-1 ring-gray-400 rounded-sm ring-1" />
+      <button>Add</button>
+    </form>
+  </Modal>
 </template>
