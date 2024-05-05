b3db86e44f83c4c0750ccfc554d8cd4b1c4ad4ca
ashrafalmayah <ashrafalmayah00@gmail.com>
2024-05-05 13:04:49 +0300

    team project dashboard, code organization

---
+++ src/components/Teams/TeamFooter.vue
@@ -0,0 +1,10 @@
+<script setup>
+defineProps({
+  team: Object
+})
+</script>
+<template>
+  <footer class="mt-12 bg-gray-800 py-4 text-center">
+    <h5 class="font-semibold text-lg">{{ team.name }} - {{ team.spots }} Member Team</h5>
+  </footer>
+</template>
--- src/views/TeamView.vue
+++ src/views/TeamView.vue
@@ -1,107 +1,16 @@
 <script setup>
+import TeamHeader from '@/components/Teams/TeamHeader.vue'
+import TeamMembers from '@/components/Teams/TeamMembers.vue'
+import TeamFooter from '@/components/Teams/TeamFooter.vue'
+import team from '@/team.json'
 </script>
 
 <template>
-  <header class="flex justify-between">
-    <div>
-      <button
-        class="bg-green-500 hover:bg-green-600 text-white px-4 py-2 rounded disabled:bg-gray-400"
-      >
-        Add Member (0 Spots Left)
-      </button>
-    </div>
-
-    <div>
-      <div class="inline-flex items-center text-3xl relative">
-        <img src="" alt="" class="mr-2" />
-        <h3>Smiley Team</h3>
-        <div
-          class="bg-green-400 w-5 h-5 text-xs text-white rounded-full flex justify-center items-center absolute -right-4 -top-2"
-        >
-          5
-        </div>
-      </div>
-    </div>
-  </header>
+  <TeamHeader :team="team" />
 
   <div class="place-self-center flex flex-col gap-y-3" style="width: 725px">
-    <table class="table-fixed border-spacing-2 border-separate">
-      <thead>
-        <th class="text-left px-6 py-2">Name</th>
-        <th class="text-left px-6 py-2">Email</th>
-        <th class="text-left px-6 py-2">Status</th>
-      </thead>
-
-      <tbody>
-        <tr class="bg-gray-800 px-12">
-          <td class="text-xl font-medium flex items-center gap-x-4 px-6 py-4">
-            <img :src="`https://i.pravatar.cc/50?u=jondoe@gmail.com`" alt="" class="rounded-xl" />
-            John Doe
-          </td>
-
-          <td class="text-gray-500 px-6 py-4">johndoe@gmail.com</td>
-
-          <td class="px-6 py-4">
-            <button class="text-green-400 text-xl">Active</button>
-          </td>
-        </tr>
-        <tr class="bg-gray-800 px-12">
-          <td class="text-xl font-medium flex items-center gap-x-4 px-6 py-4">
-            <img :src="`https://i.pravatar.cc/50?u=jondoe@gmail.com`" alt="" class="rounded-xl" />
-            John Doe
-          </td>
-
-          <td class="text-gray-500 px-6 py-4">johndoe@gmail.com</td>
-
-          <td class="px-6 py-4">
-            <button class="text-green-400 text-xl">Active</button>
-          </td>
-        </tr>
-        <tr class="bg-gray-800 px-12">
-          <td class="text-xl font-medium flex items-center gap-x-4 px-6 py-4">
-            <img :src="`https://i.pravatar.cc/50?u=jondoe@gmail.com`" alt="" class="rounded-xl" />
-            John Doe
-          </td>
-
-          <td class="text-gray-500 px-6 py-4">johndoe@gmail.com</td>
-
-          <td class="px-6 py-4">
-            <button class="text-green-400 text-xl">Active</button>
-          </td>
-        </tr>
-        <tr class="bg-gray-800 px-12">
-          <td class="text-xl font-medium flex items-center gap-x-4 px-6 py-4">
-            <img :src="`https://i.pravatar.cc/50?u=jondoe@gmail.com`" alt="" class="rounded-xl" />
-            John Doe
-          </td>
-
-          <td class="text-gray-500 px-6 py-4">johndoe@gmail.com</td>
-
-          <td class="px-6 py-4">
-            <button class="text-green-400 text-xl">Active</button>
-          </td>
-        </tr>
-        <tr class="bg-gray-800 px-12">
-          <td class="text-xl font-medium flex items-center gap-x-4 px-6 py-4">
-            <img :src="`https://i.pravatar.cc/50?u=jondoe@gmail.com`" alt="" class="rounded-xl" />
-            John Doe
-          </td>
-
-          <td class="text-gray-500 px-6 py-4">johndoe@gmail.com</td>
-
-          <td class="px-6 py-4">
-            <button class="text-green-400 text-xl">Active</button>
-          </td>
-        </tr>
-      </tbody>
-    </table>
-
-    <p class="text-right text-gray-600 italic">
-      There are no remaining team spots. Upgrade to add more.
-    </p>
+    <TeamMembers :team="team" />
   </div>
 
-  <footer class="mt-12 bg-gray-800 py-4 text-center">
-    <h5 class="font-semibold text-lg">Smiley - 5 Member Team</h5>
-  </footer>
+  <TeamFooter :team="team" />
 </template>
